19d4b799e932bba43b3c1d485b04213da91cb334
339713067 <339713067@2260I-L29-023>
2025-09-04 09:38:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace The_basic_assigments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName = "North";
+            String favMvie = "Matrix";
+            Console.WriteLine("My name is " + FirstName.ToLower() + " and my favorite movie is " + favMvie);
+            Console.WriteLine(favMvie.Contains("The"));
+
+        }
+    }
+}
